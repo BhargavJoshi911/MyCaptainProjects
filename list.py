eed67f09d0cdfaf6f60cde1e6197e9942e80a777
BhargavJoshi911 <bhargavjoshi159@gmail.com>
2021-03-25 20:19:40 +0530

    first commit

---
+++ list.py
@@ -0,0 +1,14 @@
+myList1 = [12, -7, 5, 64, -14]
+myList2 = [12, 14, -95, 3]
+
+print("positive numbers in List 1")
+for number in myList1:
+    if number > 0:
+        print(number)
+
+print('\n')
+
+print("Positive numbers in List 2")
+for number in myList2:
+    if number > 0:
+        print(number)
